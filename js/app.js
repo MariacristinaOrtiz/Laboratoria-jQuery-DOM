f787254311005e6b5246ef6ce2cdba318d625944
MariacristinaOrtiz <mari_d17@hotmail.com>
2017-12-21 15:53:17 -0500

    añade modificaciones

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
-jQuery(document).ready(function() {
+/*jQuery(document).ready(function() {
   console.log(jQuery('#p-test').text());
   console.log(jQuery('#p-test').html());
   console.log(jQuery('#input-test').val());
+});*/
+
+
+/*$(document).ready(function(){
+});*/
+$(_=> {
+console.log($('p').text('macri'));
+console.log($('p').eq(0).text());
+console.log($('p').eq(0).text('hvhvhjv'));
+$('p').eq(0).addClass('color');
+console.log($('ul').children());
+console.log($('li').eq(0));
+console.log($('li'));
+$('div').attr('nombre-attr', 'val')/* crear atributo*//*con un parametro sirve para consultar el nombre del atributo*/
+$('div').prop('nombre-attr', 'otro valor')
+$('input').prop('value')
 });
